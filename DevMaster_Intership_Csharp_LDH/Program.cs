8238a67538a50b9f66aab9dc1961767f865f8427
mase2155 <dev.hung.utc@gmail.com>
2026-08-11 20:26:50 +0700

    First Commit

---
+++ DevMaster_Intership_Csharp_LDH/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DevMaster_Intership_Csharp_LDH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

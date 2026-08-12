648b3fe326880511426296926d082cc0bf9ff191
mase2155 <dev.hung.utc@gmail.com>
2026-08-12 15:55:51 +0700

    add Helpers folder to solve the problem of Array and Matrix

--- Ex02.ArrayProcessing/Program.cs
+++ Ex02.ArrayProcessing/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Ex02.ArrayProcessing
+﻿using System.Globalization;
+using System.Text;
+
+namespace Ex02.ArrayProcessing
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            CultureInfo culture = new CultureInfo("vi-VN");
+
+            Console.WriteLine("|==================================================|");
+            Console.WriteLine("    CHƯƠNG TRÌNH XỬ LÍ MẢNG (DEVMASTER)   ");
+            Console.WriteLine("|==================================================|\n");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("------------- Xử lí mảng một chiều  -----------");
+
+            public void ShowArray()
+        {
+            Console.Write("Hãy nhập số phần tử của mảng : ");
+            int numberOfArray = int.Parse(Console.ReadLine());
+        }
+        
+            
+
         }
     }
 }

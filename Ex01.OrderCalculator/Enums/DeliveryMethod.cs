94e82220063958a685747eb57da1a1ea3792d597
mase2155 <dev.hung.utc@gmail.com>
2026-08-12 15:04:34 +0700

    finish the Ex01

---
+++ Ex01.OrderCalculator/Enums/DeliveryMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01.OrderCalculator.Enums
+{
+    public enum DeliveryMethod
+    {
+        Standard = 1, // Giao hàng tiêu chuẩn
+        Express = 2,  // Giao hàng nhanh
+        Fast = 3      // Giao hàng hỏa tốc
+    }
+}

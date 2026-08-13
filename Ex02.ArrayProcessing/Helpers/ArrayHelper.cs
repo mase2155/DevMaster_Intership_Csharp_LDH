307c11393d1d3eb61f65d9e520a6372acfef90ac
mase2155 <dev.hung.utc@gmail.com>
2026-08-13 15:58:51 +0700

    finish method 1 to 6 of ArrrayHelpers

--- Ex02.ArrayProcessing/Helpers/ArrayHelper.cs
+++ Ex02.ArrayProcessing/Helpers/ArrayHelper.cs
@@ -8,5 +8,131 @@ namespace Ex02.ArrayProcessing.Helpers
 {
      class ArrayHelper
     {
+        public void ShowArray(int[] array)
+        {
+
+            foreach ( int number in array)
+            {
+                Console.Write(number + "  ");
+            }
+            
+        }
+        public int SumArray(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0;i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        public int AverageArray(int[] array)
+        {
+            int step = 0;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                step += i;
+            }
+            return SumArray(array) / step;
+        }
+        public int MaxInArray(int[] array)
+        {
+            int max = array[0];
+            for(int i = 0; i < array.Length ; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+        public int MinInArray(int[] array)
+        {
+            int min = array[0];
+            for ( int i = 0; i < array.Length; i++)
+            {
+                if ( array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        // Hàm đếm số chẵn ( Count Even Numbers )
+        public int CountEven(int[] array)
+        {
+            int even = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 ==0)
+                {
+                    even++;
+                }
+            }
+            return even; 
+        }
+        // Hàm đếm số lẻ ( Count Odd Numbers )
+        public int CountOdd(int[] array)
+        {
+            int odd = 0;
+            for ( int j = 0; j < array.Length; j++)
+            {
+                if (array[j] % 2 != 0)
+                {
+                    odd++;
+                }
+            }
+            return odd;
+        }
+        public bool CheckPrimeNumbers(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) // 
+                {
+                    return false; 
+                }
+            }
+            return true;
+        }
+        public void PrintPrimeNumbers(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (CheckPrimeNumbers(array[i]) == true)
+                {
+                    Console.Write(array[i] + "  ");
+                }
+            }
+            Console.WriteLine();
+        }
+        public void SortArray(int[] array)
+        {
+            for (int i = 0;i < array.Length-1; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+        public void PrintSortArray(int[] array)
+        {
+           for(int i = 0;i < array.Length;i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
     }
 }
--- Ex02.ArrayProcessing/Program.cs
+++ Ex02.ArrayProcessing/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.Text;
+using Ex02.ArrayProcessing.Helpers;
 
 namespace Ex02.ArrayProcessing
 {
@@ -21,14 +22,52 @@ namespace Ex02.ArrayProcessing
 
             Console.WriteLine("------------- Xử lí mảng một chiều  -----------");
 
-            public void ShowArray()
-        {
+            Console.WriteLine("---- 1. Hiển thị mảng  ");
             Console.Write("Hãy nhập số phần tử của mảng : ");
             int numberOfArray = int.Parse(Console.ReadLine());
-        }
-        
-            
+            while (numberOfArray <= 0)
+            {
+                Console.WriteLine("Có lỗi với số lượng phần tử bạn vừa nhập.");
+                Console.Write("Hãy nhập lại: ");
+                numberOfArray = int.Parse(Console.ReadLine()); 
+            }
+            int[] array = new int[numberOfArray];
+            for(int i = 0;i< array.Length;i++)
+            {
+                Console.Write("Phần tử của mảng là : ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Vậy mảng hiện tại là : ");
+            ArrayHelper arrayToUse = new ArrayHelper();
+            arrayToUse.ShowArray(array);
 
+            //Console.WriteLine();
+            //Console.WriteLine("---- 2. Tính tổng các phần tử   ");
+            //Console.Write("Tổng các phần tử của mảng đó là : " + arrayToUse.SumArray(array));
+
+            //Console.WriteLine();
+            //Console.WriteLine("---- 3. Tính trung bình cộng của các phần tử   ");
+            //Console.Write("Trung bình cộng của các phần tử trong mảng là : " + arrayToUse.AverageArray(array));
+
+            //Console.WriteLine();
+            //Console.WriteLine("---- 4. Tìm phần tử lớn nhất và nhỏ nhất   ");
+            //Console.Write("Phần tử lớn nhất trong mảng là : " + arrayToUse.MaxInArray(array));
+            //Console.WriteLine();
+            //Console.Write("Phần tử bé nhất trong mảng là: " + arrayToUse.MinInArray(array));
+
+            //Console.WriteLine();
+            //Console.WriteLine("---- 5. Đếm số chẵn và lẻ    ");
+            //Console.Write("Số lượng phần tử là số chẵn là : " + arrayToUse.CountEven(array));
+            //Console.WriteLine();
+            //Console.Write("Số lượng phần tử là số lẻ là :  " + arrayToUse.CountOdd(array));
+
+            //Console.WriteLine();
+            //Console.WriteLine("---- 6. Liệt kê các số nguyên tố   ");
+            //Console.Write("Các phần tử là số nguyên tố là : ");
+            //arrayToUse.PrintPrimeNumbers(array);
+
+            Console.WriteLine();
+            Console.WriteLine("---- 7. Sắp xếp tăng dần    ");
         }
     }
 }

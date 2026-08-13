cf784a4ee75830ecdefceddb92204ea512f98d4f
mase2155 <dev.hung.utc@gmail.com>
2026-08-13 19:04:32 +0700

    create class for Enums

---
+++ Ex04.StudentManagement/Enums/StudentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.StudentManagement.Enums
+{
+    public enum StudentStatus
+    {
+        Studying = 1 ,
+        Reserved = 2,
+        Graduated = 3,
+        DroppedOut = 4
+    }
+}
